9ee3dfd83b5825836e22fad7d0c8178e0211b1bb
anoopmundathan <anoop.mundathan@gmail.com>
2017-01-13 15:41:26 +0000

    Create node server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-node_modules
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+	res.end('Hello World');
+});
+
+server.listen(8000, function() {
+	console.log('Server running at port 8000');
+});
